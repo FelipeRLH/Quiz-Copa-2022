79d0fe3cf46ad5734f1f269ce2ab2cf6ba08239c
Felipe Ramalho Alves <feliperamalhoalves40@gmail.com>
2022-12-21 14:00:21 -0300

    first commit

---
+++ src/data/questions.js
@@ -0,0 +1,55 @@
+const data = [
+  {
+    question: "Quem Ganhou a copa do mundo de 2022?",
+    options: ["Brasil", "Argentina", "Espanha", "França", "Marrocos",],
+    answer: "Argentina",
+  },
+  {
+    question: "Quem foi o artilheiro da copa de 2022?",
+    options: ["Messi", "Modric", "Cristiano Ronaldo", "Mbappe", "Neymar"],
+    answer: "Mbappe",
+  },
+  {
+    question: "A abertura da copa foi em qual dia?",
+    options: ["10/11/2022", "20/11/2022", "21/11/2022", "20/12/22"],
+    answer: "20/11/2022",
+  },
+  {
+    question: "Qual nome do mascote da copa do mundo?",
+    options: ["Richarlison", "La'eeb", "Canarinho", "Fuleco", "Willie"],
+    answer: "La'eeb",
+  },
+  {
+    question: "A seleção brasileira foi eliminada por quem?",
+    options: ["Croacia", "Portugal", "Alemanha", "Suiça", "Bélgica"],
+    answer: "Croacia",
+  },
+  {
+    question: "Quem fez hat-trick na final da copa de 2022?",
+    options: ["Modric", "ninguém", "Klose", "Messi", "Mbappe"],
+    answer: "Mbappe",
+  },
+  {
+    question: "Qual foi a maior goleada dessa copa?",
+    options: ["Espanha 7x0 Costa Rica", "França 10x0 Marrocos", "Argentina 3x0 Croacia"],
+    answer: "Espanha 7x0 Costa Rica",
+  },
+  {
+    question: "Quais seleções passaram para as oitavas do grupo H?",
+    options: ["Coreia do sul e Uruguai", "Gana e Portugal", "Portugal e Coreia do sul", "Portugal e Uruguai"],
+    answer: "Portugal e Coreia do sul",
+  },
+  {
+    question: "Qual dia foi a final da copa 2022?",
+    options: ["19/12/2022", "18/11/2022", "18/12/2022", "17/12/2022", "17/11/2022"],
+    answer: "18/12/2022",
+  },
+  {
+    question: "Qual foi o país sede da copa 2022?",
+    options: ["Croacia", "Catar", "Alemanha", "EUA", "Canadá"],
+    answer: "Catar",
+  },
+  
+];
+
+export default data;
